82bd1f62113d792caecd77446aaefb9050988111
Kamogelo Mahlake <3949843@myuwc.ac.za>
2026-05-03 17:03:38 +0200

    Add: singlyLinkedList - submission-8

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-8.py
@@ -0,0 +1,80 @@
+class Node:
+    def __init__(self, value, next = None):
+        self.value = value
+        self.next = next
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def get(self, index: int) -> int:
+        currentNode = self.head
+        currentIndex = 0
+
+        while currentNode:
+            if currentIndex == index:
+                return currentNode.value
+            currentIndex += 1
+            currentNode = currentNode.next
+        return -1
+
+    def insertHead(self, val: int) -> None:
+        node = Node(val, self.head)
+        self.head = node
+        self.length += 1
+
+        if self.length == 1:
+            self.tail = node
+
+    def insertTail(self, val: int) -> None:
+        node = Node(val, None)
+        if self.tail:
+            self.tail.next = node
+        self.tail = node
+        self.length += 1        
+        
+        if self.length == 1:
+            self.head = node
+
+    def remove(self, index: int) -> bool:
+        currentNode = self.head
+        previousNode = None
+        currentIndex = 0
+
+        while currentNode:
+            if currentIndex == index:
+                if index == 0:
+                    self.head = self.head.next
+                    
+                    if self.length == 1:
+                        self.tail = None
+
+                elif index == self.length - 1:
+                    self.tail = previousNode
+                    self.tail.next = None
+
+                else:
+                    previousNode.next = currentNode.next
+                self.length -= 1
+                return True
+
+            currentIndex += 1
+            previousNode = currentNode
+            currentNode = currentNode.next
+        
+        return False
+            
+
+    def getValues(self) -> List[int]:
+        array = []
+
+        currentNode = self.head
+
+        while currentNode:
+            array.append(currentNode.value)
+            currentNode = currentNode.next
+
+        return array
